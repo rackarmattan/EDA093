581d7f90c51a95540f83f26ca661009846422c43
Jakob <jholmgren95@gmail.com>
2020-10-12 14:12:27 +0200

    lOcK

--- lab3/pintos/src/devices/batch-scheduler.c
+++ lab3/pintos/src/devices/batch-scheduler.c
@@ -14,39 +14,50 @@
 #define NORMAL 0
 #define HIGH 1
 
+static int numTasksOnBus;
+static int currentDirection;
+static struct condition tasksWaitingToGo[2];
+static struct condition highPrioWaitingToGo[2];
+static int waiters[2];
+static struct lock lock1;
+
 /*
  *	initialize task with direction and priority
  *	call o
  * */
-typedef struct {
-	int direction;
-	int priority;
+typedef struct
+{
+    int direction;
+    int priority;
 } task_t;
 
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
-        unsigned int num_priority_send, unsigned int num_priority_receive);
+                    unsigned int num_priority_send, unsigned int num_priority_receive);
 
 void senderTask(void *);
 void receiverTask(void *);
 void senderPriorityTask(void *);
 void receiverPriorityTask(void *);
 
+void oneTask(task_t task);      /*Task requires to use the bus and executes methods below*/
+void getSlot(task_t task);      /* task tries to use slot on the bus */
+void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
+void leaveSlot(task_t task);    /* task release the slot */
 
-void oneTask(task_t task);/*Task requires to use the bus and executes methods below*/
-	void getSlot(task_t task); /* task tries to use slot on the bus */
-	void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
-	void leaveSlot(task_t task); /* task release the slot */
+/* initializes semaphores */
+void init_bus(void)
+{
 
-
-
-/* initializes semaphores */ 
-void init_bus(void){ 
- 
-    random_init((unsigned int)123456789); 
-    
-    msg("NOT IMPLEMENTED");
-    /* FIXME implement */
-
+    random_init((unsigned int)123456789);
+    cond_init(&tasksWaitingToGo[0]);
+    cond_init(&tasksWaitingToGo[1]);
+    cond_init(&highPrioWaitingToGo[0]);
+    cond_init(&highPrioWaitingToGo[1]);
+    lock_init(&lock1);
+    waiters[0] = 0;
+    waiters[1] = 0;
+    numTasksOnBus = 0;
+    currentDirection = 0;
 }
 
 /*
@@ -61,75 +72,116 @@ void init_bus(void){
  */
 
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
-        unsigned int num_priority_send, unsigned int num_priority_receive)
+                    unsigned int num_priority_send, unsigned int num_priority_receive)
 {
     unsigned int i;
     /* create sender threads */
-    for(i = 0; i < num_tasks_send; i++)
+    for (i = 0; i < num_tasks_send; i++)
         thread_create("sender_task", 1, senderTask, NULL);
 
     /* create receiver threads */
-    for(i = 0; i < num_task_receive; i++)
+    for (i = 0; i < num_task_receive; i++)
         thread_create("receiver_task", 1, receiverTask, NULL);
 
     /* create high priority sender threads */
-    for(i = 0; i < num_priority_send; i++)
-       thread_create("prio_sender_task", 1, senderPriorityTask, NULL);
+    for (i = 0; i < num_priority_send; i++)
+        thread_create("prio_sender_task", 1, senderPriorityTask, NULL);
 
     /* create high priority receiver threads */
-    for(i = 0; i < num_priority_receive; i++)
-       thread_create("prio_receiver_task", 1, receiverPriorityTask, NULL);
+    for (i = 0; i < num_priority_receive; i++)
+        thread_create("prio_receiver_task", 1, receiverPriorityTask, NULL);
 }
 
 /* Normal task,  sending data to the accelerator */
-void senderTask(void *aux UNUSED){
-        task_t task = {SENDER, NORMAL};
-        oneTask(task);
+void senderTask(void *aux UNUSED)
+{
+    task_t task = {SENDER, NORMAL};
+    oneTask(task);
 }
 
 /* High priority task, sending data to the accelerator */
-void senderPriorityTask(void *aux UNUSED){
-        task_t task = {SENDER, HIGH};
-        oneTask(task);
+void senderPriorityTask(void *aux UNUSED)
+{
+    task_t task = {SENDER, HIGH};
+    oneTask(task);
 }
 
 /* Normal task, reading data from the accelerator */
-void receiverTask(void *aux UNUSED){
-        task_t task = {RECEIVER, NORMAL};
-        oneTask(task);
+void receiverTask(void *aux UNUSED)
+{
+    task_t task = {RECEIVER, NORMAL};
+    oneTask(task);
 }
 
 /* High priority task, reading data from the accelerator */
-void receiverPriorityTask(void *aux UNUSED){
-        task_t task = {RECEIVER, HIGH};
-        oneTask(task);
+void receiverPriorityTask(void *aux UNUSED)
+{
+    task_t task = {RECEIVER, HIGH};
+    oneTask(task);
 }
 
 /* abstract task execution*/
-void oneTask(task_t task) {
-  getSlot(task);
-  transferData(task);
-  leaveSlot(task);
+void oneTask(task_t task)
+{
+    getSlot(task);
+    transferData(task);
+    leaveSlot(task);
 }
 
-
 /* task tries to get slot on the bus subsystem */
-void getSlot(task_t task) 
+// TODO: Add priority
+void getSlot(task_t task)
 {
-    msg("NOT IMPLEMENTED");
-    /* FIXME implement */
+    lock_acquire(&lock1);
+    while (numTasksOnBus == 3 || (numTasksOnBus > 0 && currentDirection != task.direction))
+    {
+        waiters[task.direction]++;
+        if (task.priority == 1)
+        {
+            cond_wait(&highPrioWaitingToGo[task.direction], &lock1);
+        }
+        else
+        {
+            cond_wait(&tasksWaitingToGo[task.direction], &lock1);
+        }
+        waiters[task.direction]--;
+    }
+    numTasksOnBus++;
+    currentDirection = task.direction;
+    lock_release(&lock1);
 }
 
 /* task processes data on the bus send/receive */
-void transferData(task_t task) 
+void transferData(task_t task)
 {
-    msg("NOT IMPLEMENTED");
-    /* FIXME implement */
+    unsigned long rand = random_ulong();
+    int64_t ticks = (int64_t)rand % 50;
+    timer_sleep(ticks);
 }
 
 /* task releases the slot */
-void leaveSlot(task_t task) 
+void leaveSlot(task_t task)
 {
-    msg("NOT IMPLEMENTED");
-    /* FIXME implement */
+    lock_acquire(&lock1);
+    numTasksOnBus--;
+    if (!list_empty(&highPrioWaitingToGo[currentDirection].waiters))
+    {
+        cond_signal(&highPrioWaitingToGo[currentDirection], &lock1);
+    }
+    else if (!list_empty(&highPrioWaitingToGo[1 - currentDirection]))
+    {
+        cond_signal(&highPrioWaitingToGo[1 - currentDirection], &lock1);
+    }
+    else
+    {
+        if (waiters[currentDirection] > 0)
+        {
+            cond_signal(&tasksWaitingToGo[currentDirection], &lock1);
+        }
+        else if (numTasksOnBus == 0)
+        {
+            cond_broadcast(&tasksWaitingToGo[1 - currentDirection], &lock1);
+        }
+    }
+    lock_release(&lock1);
 }
